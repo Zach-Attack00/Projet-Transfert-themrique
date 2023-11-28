625124d70ca606c18ccc7bdd1dde31e4f03f59ba
zachattack.logiq <logiq.zachattack@gmail.com>
2023-11-28 11:45:24 -0500

    conciergerie

---
+++ data_cleaning.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+def Concierge():
+    empty_data = []
+    df = pd.read_csv("data_brutes.csv")
+    for headers in df.colums[:]:
+        if headers != "DATE":
+            data = df[headers]
+            if headers != "Text":
+                array = []
+                for i in range(1, len(data)):
+                    array.append(float(data[i].strip().replace(",", ".")))
+                empty_data.append(array)
+            if headers == "Text":
+                ar = []
+                for k in range(1, len(data)):
+                    array_text.append(data[k])
+                empty_data.insert(2, array_text)
+        if headers == "DATE":
+            time = df["DATE"]
+            array_date = []
+            for j in range(1, len(time)):
+                array_date.append(time[j])
+            empty_data.insert(0, array_date)
+    vals = {n:empty_data[k] for n, k in enumerate(df.columns)}
+    df.to_csv("data.csv", index=False)
+    
